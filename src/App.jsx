d11d623aa1f9de797db70482e774a3ce31a7c7dd
zhraa <zhraa2005spn@gmail.com>
2023-07-30 14:29:35 +0300

    made quiz

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import React from 'react';
+import Quize from './Quiz'
+function App() {
+  return (
+    <div className="App">
+      < Quize/>
+    </div>
+  );
+}
+
+export default App;
